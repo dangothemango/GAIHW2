704fbb6570d845890bfd1fe889b0acf5d059f744
Dan Gorman <danthemangorman@gmail.com>
2017-09-23 13:43:48 -0400

    added agent file

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+
+    public enum State {
+        wait,
+        wander,
+        pursue,
+        evade,
+        path
+    }
+
+    public State curState = State.wait;
+    public GameObject target;
+    public Transform[] path;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (curState) {
+            case State.wait:
+                break;
+            case State.wander:
+                Wander();
+                break;
+            case State.pursue:
+                Pursue();
+                break;
+            case State.evade:
+                Evade();
+                break;
+            case State.path:
+                FollowPath();
+                break;
+        }
+	}
+
+    void Wander() {
+
+    }
+
+    void Pursue() {
+
+    }
+
+    void Evade() {
+
+    }
+
+    void FollowPath() {
+
+    }
+}
